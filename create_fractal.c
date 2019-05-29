95dd3bf9e65452405291e52c57246ad31b5eff07
mattip <matti.picus@gmail.com>
2019-05-29 23:49:04 +0300

    add windows dll, exe

--- create_fractal.c
+++ create_fractal.c
@@ -3,15 +3,16 @@
 int create_fractal(Img img,  int iters) {
     float pixel_size_x = 3.0 / img.width;
     float pixel_size_y = 2.0 / img.height;
-    int x, y;
+    int x, y, ret=0;
     for (y=0; y < img.height; y++) {
         float imag = y * pixel_size_y - 1;
         int yy = y * img.width;
         for (x=0; x < img.width; x++) {
             float real = x * pixel_size_x - 2;
             unsigned char color;
-            int ret = mandel(real, imag, iters, &color);
+            ret += mandel(real, imag, iters, &color);
             img.data[yy + x] = color;
         }
     }
+    return ret;
 }
