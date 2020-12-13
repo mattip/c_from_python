b6636735327cd3827bf90bae53719aa346a56cc9
^^ Bianca ^^ <73293685+XxBiancaXx@users.noreply.github.com>
2020-12-13 01:21:34 +0100

    Update fractal_ctypes.py
    Changed .format() into an FString to make the code easier to read and modify and made the code more clean.
    
    Nothing big, but this can be usefull later on ^^

--- fractal_ctypes.py
+++ fractal_ctypes.py
@@ -13,15 +13,19 @@ class CtypesImg(ctypes.Structure):
                 ('height', ctypes.c_int),
                 ('data', ctypes.POINTER(ctypes.c_uint8)), # HUH?
                ]
+    
     array_cache = {}
+    
     def __init__(self, width, height):
         self.width = width
         self.height = height
         # Create a class type to hold the data.
         # Since this creates a type, cache it for reuse rather
         # than create a new one each time
+        
         if width*height not in self.array_cache:
             self.array_cache[width*height] = ctypes.c_uint8 * (width * height)
+            
         # Note this keeps the img.data alive in the interpreter
         self.data = self.array_cache[width*height]() # !!!!!!
 
@@ -29,13 +33,14 @@ class CtypesImg(ctypes.Structure):
         # There must be a better way, but this code will not
         # be timed, so explicit trumps implicit
         ret = self.array_cache[width*height]()
+        
         for i in range(width*height):
             ret[i] = self.data[i]
+            
         return ret
 
 ctypesimg = CtypesImg(width, height)
 
-    
 # Load the DLL
 cdll = ctypes.cdll.LoadLibrary('./libcreate_fractal.so')
 
@@ -47,16 +52,17 @@ mandel_ctypes = cdll.mandel
 mandel_ctypes.argtypes = [ctypes.c_float, ctypes.c_float, ctypes.c_int, 
                           ctypes.POINTER(ctypes.c_uint8)]
 
-
 if __name__ == "__main__":
     from timeit import default_timer as timer
     from PIL import Image
     from create_fractal import create_fractal
+    
     s = timer()
     create_fractal_ctypes(ctypesimg, 20)
     e = timer()
     ctypes_onecall = e - s
-    print('ctypes calling create_fractal required {:.2f} millisecs'.format(1000*ctypes_onecall))
+    
+    print(f'ctypes calling create_fractal required {1000 * ctypes_onecall} millisecs')
     data = ctypesimg.asmemoryview()
     print(max(data))
     im = Image.frombuffer("L", (width, height), data, 'raw', 'L', 0, 1)
@@ -68,7 +74,9 @@ if __name__ == "__main__":
     e = timer()
     ctypes_createfractal = e - s
     data = ctypesimg.asmemoryview()
+    
     print(max(data))
-    print('ctypes calling mandel required {:.2f} millisecs'.format(1000*ctypes_createfractal))
+    print(f'ctypes calling mandel required {1000 * ctypes_createfractal} millisecs')
+    
     im = Image.frombuffer("L", (width, height), data, 'raw', 'L', 0, 1)
     im.save('ctypes_mandel.png')
